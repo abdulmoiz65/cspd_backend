5f768eef8774b6902cc87cadb1653708687019cf
AbdulMoiz <pc16786abdulmoiz@gmail.com>
2025-12-19 19:09:30 +0500

    first commit

---
+++ resources/views/cspd_admin/layout/app.blade.php
@@ -0,0 +1,7 @@
+@include('cspd_admin.layout.header')
+
+
+@yield('content')
+
+
+@include('cspd_admin.layout.footer')
---
+++ resources/views/cspd_admin/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends('cspd_admin.layout.app')
+
+@section('content')
+    <!-- Blank Start -->
+    <div class="container-fluid pt-4 px-4 ">
+        <div class="blank  row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+            <div class="col-md-6 text-center">
+                <h3>This is blank page</h3>
+            </div>
+        </div>
+    </div>
+    <!-- Blank End -->
+@endsection
